e0dd181066928a4ebff909e54894224a90990fc0
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2020-09-03 00:47:22 -0300

    :art: Updated Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 
 
 import Product from  '../Product';
+import Header from '../Header';
+import Footer from '../Footer';
 
 import { Container, Wrapper} from './styles';
 
 const Layout: React.FC = () => {
   return (
     <Container>
+      <Header />
       <Wrapper>
         <Product />
       </Wrapper>
+
+      <Footer />
     </Container>
 
   )
